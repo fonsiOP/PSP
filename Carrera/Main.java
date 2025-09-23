7eb6570f992e4018c5a37fc0daad4775cee336de
fonsiOP <152197044+fonsiOP@users.noreply.github.com>
2025-09-23 17:35:59 +0200

    Up

--- Carrera/Main.java
+++ Carrera/Main.java
@@ -1,8 +1,12 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Random rand = new Random();
 		
+		int num= Math.abs(rand.nextInt(3));
+		System.out.println(num);
 
 	}
 
